eaf3e07fff2ea81ea42d004bb9850857f2225d4a
Junyang <nathanhejunyang@gmail.com>
2018-09-05 15:35:14 +0200

    Update 75. Sort Colors.java

--- array/75. Sort Colors.java
+++ array/75. Sort Colors.java
@@ -14,3 +14,37 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public void sortColors(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+    
+    private void quickSort(int[] nums, int s, int e) {
+        if (s > e) {
+            return;
+        }
+        
+        int pivot = nums[s];
+        int i = s, j = e;
+        
+        while (i < j) {
+            while (i < j && nums[j] >= pivot) {
+                j--;
+            }
+            
+            nums[i] = nums[j];
+            
+            while (i < j && nums[i] < pivot) {
+                i++;
+            }
+            
+            nums[j] = nums[i];
+        }
+        
+        nums[i] = pivot;
+        
+        quickSort(nums, s, i - 1);
+        quickSort(nums, i + 1, e);
+    }
+}
