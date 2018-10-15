1e0659dbe93b25539265fd73d536087cc2d1fd6b
Junyang <nathanhejunyang@gmail.com>
2018-10-15 10:57:09 +0200

    Update 771. Jewels and Stones.java

--- hashtable/771. Jewels and Stones.java
+++ hashtable/771. Jewels and Stones.java
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/jewels-and-stones/
-class Solution {
+class Solution { // 15ms
     public int numJewelsInStones(String J, String S) {
         Set<Character> set = new HashSet();
         int res = 0;
@@ -11,6 +11,24 @@ class Solution {
         for (char c : S.toCharArray()) {
             if (set.contains(c)) {
                 res++;
+            }
+        }
+        
+        return res;
+    }
+}
+
+class Solution2 { // 10ms
+    public int numJewelsInStones(String J, String S) {
+        int res = 0;
+        char[] jArray = J.toCharArray();
+        char[] sArray = S.toCharArray();
+        
+        for (char j : jArray) {
+            for (char s : sArray) {
+                if (j == s) {
+                    res++;
+                }
             }
         }
         
