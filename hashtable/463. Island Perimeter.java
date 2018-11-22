00a69c03ec3fd08f3f51ddc0cd7f94b4926dd0d4
Junyang <nathanhejunyang@gmail.com>
2018-11-22 11:36:18 +0100

    Create 463. Island Perimeter.java

---
+++ hashtable/463. Island Perimeter.java
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/island-perimeter/
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        final int iLen = grid.length;
+        final int jLen = grid[0].length;
+        
+        if (iLen == 0 || jLen == 0) {
+            return 0;
+        }
+        
+        int res = 0;
+        
+        for (int i = 0; i < iLen; i++) {
+            for (int j = 0; j < jLen; j++) {
+                if (grid[i][j] == 1) {
+                    res += countCell(grid, i, j);
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    private int countCell(int[][] grid, int i, int j) {
+        int count = 0;
+        if (i == 0) {
+            count++;
+        } else if (grid[i - 1][j] == 0) {
+            count++;
+        }
+        
+        if (i == grid.length - 1) {
+            count++;
+        } else if (grid[i + 1][j] == 0) {
+            count++;
+        }
+        
+        if (j == 0) {
+            count++;
+        } else if (grid[i][j - 1] == 0) {
+            count++;
+        }
+        
+        if (j == grid[0].length - 1) {
+            count++;
+        } else if (grid[i][j + 1] == 0) {
+            count++;
+        }
+        
+        return count;
+    }
+}
