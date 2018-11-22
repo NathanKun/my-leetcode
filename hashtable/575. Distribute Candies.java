7973bf32b88d18e60540f722e1c8eb49ada3846b
Junyang <nathanhejunyang@gmail.com>
2018-11-22 11:58:52 +0100

    Create 575. Distribute Candies.java

---
+++ hashtable/575. Distribute Candies.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/distribute-candies/
+class Solution1 { // 127 ms
+    public int distributeCandies(int[] candies) {        
+        return Math.min(candies.length / 2, Arrays.stream(candies).boxed().collect(Collectors.toSet()).size());
+    }
+}
+
+class Solution2 { // 20 ms
+    public int distributeCandies(int[] candies) {        
+        boolean[] set = new boolean[200001];
+        int kind = 0;
+        int half = candies.length / 2;
+        
+        for (int i : candies) {
+            if (!set[i + 100000]) {
+                set[i + 100000] = true;
+                kind++;
+            }
+            
+            if (kind >= half) {
+                return kind;
+            }
+        }
+        
+        return kind;
+    }
+}
