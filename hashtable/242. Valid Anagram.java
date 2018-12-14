c7baa8d6982b3f5e9fe3ad6b1e21c56261254c66
Junyang <nathanhejunyang@gmail.com>
2018-12-14 16:42:02 +0100

    Create 242. Valid Anagram.java

---
+++ hashtable/242. Valid Anagram.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/valid-anagram/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] sMap = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            sMap[c - 'a']++;
+        }
+        
+        for (char c : t.toCharArray()) {
+            sMap[c - 'a']--;
+        }
+        
+        for (int i : sMap) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
