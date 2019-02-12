1e98269cb03857b5df70433768a6558cf4001749
Junyang <nathanhejunyang@gmail.com>
2019-02-12 16:22:32 +0100

    Create 720. Longest Word in Dictionary.java

---
+++ hashtable/720. Longest Word in Dictionary.java
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/longest-word-in-dictionary/
+class Solution { // 34 ms, 39.5 MB
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        
+        Set<String> set = new HashSet();
+        for (String s : words) {
+            set.add(s);
+        }
+        
+        String[] candidates = new String[words.length];
+        int p = 0;
+        int maxLen = 0;
+        for (int i = words.length - 1; i >= 0; i--) {
+            String w = words[i];
+            
+            if (check(set, w)) {
+                if (w.length() > maxLen) {
+                    maxLen = w.length();
+                    p = 0;
+                    candidates[p] = w;
+                } else if (w.length() == maxLen) {
+                    p++;
+                    candidates[p] = w;
+                }
+            }
+        }
+        
+        candidates = Arrays.copyOfRange(candidates, 0, p + 1);
+        Arrays.sort(candidates);
+        
+        return candidates[0];
+    }
+    
+    private boolean check(Set<String> set, String s) {
+        char[] array = s.toCharArray();
+        String sub = "";
+        
+        for (char c : array) {
+            sub += c;
+            if (!set.contains(sub)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
